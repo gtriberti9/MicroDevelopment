216485d8f72617d6e9ff7a579011205ad63cfbae
gtriberti9 <giulytriberti@gmail.com>
2025-05-08 08:03:39 -0500

    Added databases and started cleaning data and exploratory analysis

---
+++ cleaning_and_exploratory.R
@@ -0,0 +1,13 @@
+# Zixuan Zhou, Giuliana Triberti
+
+# Data_Cleaning
+
+# This table contains the result of the interview, the household 
+# factor and other household characteristics.
+household_data <- read.csv("CN_HOGARES.csv")
+
+# This table contains the socio-demographic characteristics of the 
+# household members, health status and utilization of health services, among others.
+individual_data <- read.csv("CN_RESIDENTES.csv")
+
+# 
